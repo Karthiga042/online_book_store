8ca777cbbd1b11e3e934975d6fd32076df1391e1
Karthiga042 <117564453+Karthiga042@users.noreply.github.com>
2024-01-05 10:36:51 +0530

    Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,6 +8,7 @@ import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import { faPinterest } from "@fortawesome/free-brands-svg-icons";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import NewsLetter from "../NewsLetter/NewsLetter";
+import { Link } from "react-router-dom";
 
 
 class Footer extends React.Component{
@@ -20,19 +21,19 @@ class Footer extends React.Component{
                     <div class="box-container">
                             <div class="box">
                                 <h3>extra links</h3>
-                                <a href="/cart" id="alter"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> my order </a>
-                                <a href="/dashboard"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> my account </a>
-                                <a href="/Faq" id="alter"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> FAQs </a>
-                                <a href="/Privacy"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> privacy policy </a>
-                                <a href="/terms" id="alter"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> terms or use </a>
+                                <Link to="/cart" id="alter"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> my order </Link>
+                                <Link to="/dashboard"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> my account </Link>
+                                <Link to="/Faq" id="alter"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> FAQs </Link>
+                                <Link to="/Privacy"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> privacy policy </Link>
+                                <Link to="/terms" id="alter"> <i class="fas fa-arrow-right"><FontAwesomeIcon icon={faArrowRight} /></i> terms or use </Link>
                             </div>
                             <div class="box">
                                 <h3>follow us</h3>
-                                <a href="https://www.facebook.com"> <i class="fab fa-facebook-f"><FontAwesomeIcon icon={faFacebookF} /></i> facebook </a>
-                                <a href="https://twitter.com" id="alter"> <i class="fab fa-twitter"><FontAwesomeIcon icon={faTwitter} /> </i>twitter </a>
-                                <a href="https://www.instagram.com"> <i class="fab fa-instagram"><FontAwesomeIcon icon={faInstagram} /></i> instagram </a>
-                                <a href="https://in.linkedin.com" id="alter"> <i class="fab fa-linkedin"><FontAwesomeIcon icon={faLinkedin} /></i> linkedin </a>
-                                <a href="https://in.pinterest.com"> <i class="fab fa-pinterest"><FontAwesomeIcon icon={faPinterest} /></i> pinterest </a>
+                                <Link to="https://www.facebook.com"> <i class="fab fa-facebook-f"><FontAwesomeIcon icon={faFacebookF} /></i> facebook </Link>
+                                <Link to="https://twitter.com" id="alter"> <i class="fab fa-twitter"><FontAwesomeIcon icon={faTwitter} /> </i>twitter </Link>
+                                <Link to="https://www.instagram.com"> <i class="fab fa-instagram"><FontAwesomeIcon icon={faInstagram} /></i> instagram </Link>
+                                <Link to="https://in.linkedin.com" id="alter"> <i class="fab fa-linkedin"><FontAwesomeIcon icon={faLinkedin} /></i> linkedin </Link>
+                                <Link to="https://in.pinterest.com"> <i class="fab fa-pinterest"><FontAwesomeIcon icon={faPinterest} /></i> pinterest </Link>
                             </div>
                     </div>
                 </section>
