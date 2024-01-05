65d57fb6e2d20ae8d77f0af82b7d85136ed3859c
Karthiga042 <117564453+Karthiga042@users.noreply.github.com>
2024-01-05 10:36:09 +0530

    Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,28 +3,14 @@ import './Hero.css';
 // import hand_icon from '../Assets/hand_icon.png'
 // import arrow_icon from '../Assets/arrow_icon.png'
 import hero_image from '../Assets/hero_image.jpg'
+import { Link } from 'react-router-dom';
 
 const Hero=()=>
 {
     return(
         <div className='hero-right'>
-            {/* <div className='hero-left'>
-                <h2>NEW ARRIVALS ONLY</h2>
-                <div>
-                <div className="hand-hand-icon">
-                    <img src={hand_icon} alt="" />
-                </div>
-                <p> New Collections of Books</p>
+            <Link to="/newarrivals"><img src={hero_image} alt="" /></Link> 
             </div>
-            <div className='hero-latest-button'>
-                <div>Latest Collection</div>
-                <img src={arrow_icon} alt="" />
-            </div>
-            </div>
-            <div className='hero-right'> */}
-            <a href="/newarrivals"><img src={hero_image} alt="" /></a> 
-            </div>
-        // </div>
     )
 }
 export default Hero
